86c8b50e1b89737d9bd1a769c1622182c6d65c03
Michael Braha <michaelbraha12@gmail.com>
2016-05-04 17:59:05 -0400

    Create divider for RecyclerViews

---
+++ app/src/main/java/com/michaelbraha/popular_movies/SimpleDividerItemDecoration.java
@@ -0,0 +1,41 @@
+package com.michaelbraha.popular_movies;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/* Code provided by StackOverflow answer here:
+*  http://stackoverflow.com/questions/31242812/how-to-add-divider-line-in-recyclerview-in-android
+*  This was deemed a better solution because it uses a drawable as divider that we can customize,
+*  in our case, to get it thicker
+*/
+public class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
+
+    private Drawable mDivider;
+
+    public SimpleDividerItemDecoration(Context context) {
+        mDivider = ContextCompat.getDrawable(context, R.drawable.line_divider);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+}
